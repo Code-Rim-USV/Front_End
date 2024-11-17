fcddee8ae1f62ae2df1599ec013c376ab1895b49
Stefan22950 <stefan.rosca@student.usv.ro>
2024-11-17 13:34:40 +0200

    Create components for the exam request form
    -Created the dropdown style component
    -Created the date selection component

---
+++ src/components/ExamRequestDateInput.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <label :for="label" class="exam-request-date-label">{{ label }}</label>
+        <input id="exam-request-date-input" type="date" v-model="selectedDate" :placeholder="placeholder"
+               class="exam-request-date-input" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ExamRequestDateInput",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      default: "",
+      validator(value) {
+        // Date format (yyyy-mm-dd)
+        const regex = /^\d{4}-\d{2}-\d{2}$/;
+        return regex.test(value);
+      }
+    },
+    placeholder: {
+      type: String,
+      default: "Select a date",
+    },
+  },
+  computed: {
+    selectedDate: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+    .exam-request-date-input {
+        width: 100%;
+        height: 80px;
+        padding: 10px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        font-size: 16px;
+    }
+
+    .exam-request-date-label {
+        font-size: 14px;
+        margin-bottom: 8px;
+        display: block;
+        font-weight: bold;
+    }
+
+    .exam-request-date-input:focus {
+        border-color: #157def;
+        outline: none;
+    }
+</style>
---
+++ src/components/ExamRequestDropInput.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <label :for="label" class="exam-request-drop-label">{{ label }}</label>
+        <select :id="label" v-model="selected" class="exam-request-drop-input">
+            <option disabled value="">{{ placeholder }}</option>
+            <option v-for="option in options" :key="option.value" :value="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ExamRequestDropInput",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+      validator(value) {
+        return value.every(option => option.hasOwnProperty('value') && option.hasOwnProperty('text'));
+      },
+    },
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "Selecteaza o optiune",
+    },
+  },
+  computed: {
+    selected: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+    .exam-request-drop-label {
+        font-size: 14px;
+        margin-bottom: 8px;
+        display: block;
+        font-weight: bold;
+    }
+
+    .exam-request-drop-input {
+        width: 100%;
+        padding: 10px;
+        font-size: 16px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        height: 80px;
+        appearance: none;
+    }
+
+        .exam-request-drop-input:focus {
+            border-color: #157def;
+            outline: none;
+        }
+</style>
